83f477f45706cde5cf85010210ffa6ccdba1a5e1
kenjilam92 <kenji.lam.92@gmail.com>
2020-07-06 18:30:55 -0400

    change img name

---
+++ client/build/precache-manifest.f9e5c06fd0b0ec06ca614679347d3830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc486d6c00f189e775d06e3f167ff5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01f4aeaed2b6b7129bf5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "76729f9d2a3c49be197f",
+    "url": "/static/css/main.8dff0997.chunk.css"
+  },
+  {
+    "revision": "01f4aeaed2b6b7129bf5",
+    "url": "/static/js/2.b2b14929.chunk.js"
+  },
+  {
+    "revision": "1dde4a451cfa0dafbd2c6d3923a8362d",
+    "url": "/static/js/2.b2b14929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76729f9d2a3c49be197f",
+    "url": "/static/js/main.b6c53006.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
