b2de824c667543ea85a65c2e32ce15fff679f17b
kenjilam92 <kenji.lam.92@gmail.com>
2020-07-22 01:09:29 -0400

    new build

---
+++ client/build/precache-manifest.5968510ca41d7e09a9b22addd543376b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11d04b9e8e8e8d142028aee991335c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82acbc3d495e1880d7d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "024d685b4968792e06a8",
+    "url": "/static/css/main.8dff0997.chunk.css"
+  },
+  {
+    "revision": "e82acbc3d495e1880d7d",
+    "url": "/static/js/2.7fbec385.chunk.js"
+  },
+  {
+    "revision": "1dde4a451cfa0dafbd2c6d3923a8362d",
+    "url": "/static/js/2.7fbec385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024d685b4968792e06a8",
+    "url": "/static/js/main.12d90031.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
